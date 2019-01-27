5facaa13e0c975d929082eb97527d225c169a549
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 13:45:36 +0300

    Урок 2 - Анимация, движение картинки

--- core/src/glimantony/gmail/StarGame.java
+++ core/src/glimantony/gmail/StarGame.java
@@ -5,17 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Texture background;
 
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		background = new Texture("backgrounds/spase_stars_background.jpg");
+
 	}
 
 	@Override
@@ -24,8 +28,10 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background, 0, 0);
-		batch.draw(img, 0, 0);
+
 		batch.end();
+
+
 	}
 
 	@Override
--- core/src/glimantony/gmail/screen/MenuScreen.java
+++ core/src/glimantony/gmail/screen/MenuScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 import glimantony.gmail.base.Base2DScreen;
 
@@ -16,12 +17,18 @@ public class MenuScreen extends Base2DScreen {
     Texture img;
     Texture background;
 
+    Vector2 imgPosition; //позицыя картинки
+    Vector2 imgSpeed; //скорость картинки
+
     @Override
     public void show() { //проводим всю инициализацию
         super.show();
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
         background = new Texture("backgrounds/spase_stars_background.jpg");
+
+        imgPosition = new Vector2(10f, 10f);
+        imgSpeed = new Vector2(1f, 1f);
     }
 
     @Override
@@ -31,8 +38,18 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(background, 0, 0);
-        batch.draw(img, 0, 0);
+        batch.draw(img, imgPosition.x, imgPosition.y);
         batch.end();
+
+        //картинка дойдет до края и остановится
+        //Gdx.graphics.getWidth() - ширина экрана
+        //256 - высота и ширина картинки
+        if(Gdx.graphics.getWidth() - 256 > imgPosition.x &&
+                Gdx.graphics.getHeight() - 256 > imgPosition.y) {
+            imgPosition.add(imgSpeed); //изменение позиции на величину скорости
+        }
+
+
     }
 
     @Override
