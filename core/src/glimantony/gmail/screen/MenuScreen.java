652c1fdce494e0ca01bac9089b9e67ec085ea437
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 13:15:05 +0300

    Урок 2 - Проработан класс MenuScreen в него перенесена реализация из класса StarGame

--- core/src/glimantony/gmail/screen/MenuScreen.java
+++ core/src/glimantony/gmail/screen/MenuScreen.java
@@ -1,5 +1,10 @@
 package glimantony.gmail.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import glimantony.gmail.base.Base2DScreen;
 
 /**
@@ -7,4 +12,33 @@ import glimantony.gmail.base.Base2DScreen;
  */
 public class MenuScreen extends Base2DScreen {
 
+    SpriteBatch batch;
+    Texture img;
+    Texture background;
+
+    @Override
+    public void show() { //проводим всю инициализацию
+        super.show();
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        background = new Texture("backgrounds/spase_stars_background.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        super.dispose(); //Вызывается в конце
+    }
 }
