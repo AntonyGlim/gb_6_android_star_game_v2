24801f6fbcae7d9ca09972baf822ec3164397f5f
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 19:26:01 +0300

    Урок 4 - Рефактеринг MenuScreen (удалено лишнее)

--- core/src/glimantony/gmail/screen/MenuScreen.java
+++ core/src/glimantony/gmail/screen/MenuScreen.java
@@ -14,28 +14,12 @@ public class MenuScreen extends Base2DScreen {
 
     public static final float V_LEN = 0.001f; //величина вектора скорости
 
-    Texture img;
     Texture background;
-
-    Vector2 imgPosition; //позицыя картинки
-    Vector2 imgSpeed; //скорость картинки
-
-    Vector2 touch; //поместим сюда touch из суперкласса
-    Vector2 copyTouch; //копия того места, куда нажали ЛКМ
-
 
     @Override
     public void show() { //проводим всю инициализацию
         super.show();
-        img = new Texture("badlogic.jpg");
-        background = new Texture("backgrounds/spase_stars_background.jpg");
-
-        imgPosition = new Vector2(0, 0);
-        imgSpeed = new Vector2(0, 0);
-
-        touch = new Vector2();
-        copyTouch = new Vector2();
-
+        background = new Texture("textures/backgrounds/spase_stars_background.jpg");
     }
 
     @Override
@@ -44,29 +28,19 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        copyTouch.set(touch);
-        if (copyTouch.sub(imgPosition).len() > V_LEN){
-            imgPosition.add(imgSpeed);
-        } else {
-            imgPosition.set(touch);
-        }
-
         batch.begin();
-        batch.draw(background, -0.5f, -0.5f, 1f, 1f); //с шириной и высотой картинки
-        batch.draw(img, imgPosition.x, imgPosition.y, 0.5f, 0.5f); //с шириной и высотой картинки
+
         batch.end();
     }
 
     @Override
     public void dispose() {
-        img.dispose();
+        background.dispose();
         super.dispose(); //Вызывается в конце
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
-        this.touch = touch;
-        imgSpeed.set(touch.cpy().sub(imgPosition).setLength(V_LEN)); //находим расстояние между О-ом и нажатием и устонавливаем ск-ть (setLength)
         return super.touchDown(touch, pointer);
     }
 }
