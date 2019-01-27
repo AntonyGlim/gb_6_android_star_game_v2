c58707b6b14cd98d06f38464ea0e65f730e6f0de
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 15:11:31 +0300

    2 Урок - Выполнено ДЗ

--- core/src/glimantony/gmail/screen/MenuScreen.java
+++ core/src/glimantony/gmail/screen/MenuScreen.java
@@ -13,12 +13,17 @@ import glimantony.gmail.base.Base2DScreen;
  */
 public class MenuScreen extends Base2DScreen {
 
+    public static final float V_LEN = 0.5f; //величина вектора скорости
+
     SpriteBatch batch;
     Texture img;
     Texture background;
 
     Vector2 imgPosition; //позицыя картинки
     Vector2 imgSpeed; //скорость картинки
+
+    Vector2 userTouch; //координаты, куда нажал пользователь
+    Vector2 userTouchCopy; //координаты, куда нажал пользователь (копия для метода render)
 
     @Override
     public void show() { //проводим всю инициализацию
@@ -27,8 +32,10 @@ public class MenuScreen extends Base2DScreen {
         img = new Texture("badlogic.jpg");
         background = new Texture("backgrounds/spase_stars_background.jpg");
 
-        imgPosition = new Vector2(10f, 10f);
-        imgSpeed = new Vector2(1f, 1f);
+        imgPosition = new Vector2(0, 0);
+        imgSpeed = new Vector2(0, 0);
+        userTouch = new Vector2(0, 0);
+        userTouchCopy = new Vector2(0, 0);
     }
 
     @Override
@@ -36,19 +43,22 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        /*Если расстояние между картинкой и местом нажатия ЛКМ больше в-ра ск-ти
+        * userTouch.cpy() - создает новые экземпляры, в методе render он не допустим.
+        * Вместо него используем userTouchCopy*/
+        userTouchCopy.set(userTouch);
+        if (userTouchCopy.sub(imgPosition).len() > V_LEN){
+            imgPosition.add(imgSpeed); //изменение позиции на величину скорости
+        }
+        else {
+            imgPosition.set(userTouch); //картинка остановиться в указанных координатах
+        }
+
         batch.begin();
         batch.draw(background, 0, 0);
         batch.draw(img, imgPosition.x, imgPosition.y);
         batch.end();
-
-        //картинка дойдет до края и остановится
-        //Gdx.graphics.getWidth() - ширина экрана
-        //256 - высота и ширина картинки
-        if(Gdx.graphics.getWidth() - 256 > imgPosition.x &&
-                Gdx.graphics.getHeight() - 256 > imgPosition.y) {
-            imgPosition.add(imgSpeed); //изменение позиции на величину скорости
-        }
-
 
     }
 
@@ -67,6 +77,8 @@ public class MenuScreen extends Base2DScreen {
                 "; screenY = " + (Gdx.graphics.getHeight() - screenY) +
                 "; pointer = " + pointer +
                 "; button = " + button); //залогируем для информации о вызове метода
+        userTouch.set(screenX, (Gdx.graphics.getHeight() - screenY)); //запоминаем куда нажал пользователь
+        imgSpeed.set(userTouch.cpy().sub(imgPosition).setLength(V_LEN)); //устанавливаем вектор скорости
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
