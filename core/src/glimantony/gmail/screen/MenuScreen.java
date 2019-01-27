a1767348874ecea7c8e7b9d97eba5a7409341206
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 18:54:02 +0300

    Урок 3 - Добавлены текстуры и атласы

--- core/src/glimantony/gmail/screen/MenuScreen.java
+++ core/src/glimantony/gmail/screen/MenuScreen.java
@@ -3,7 +3,6 @@ package glimantony.gmail.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 import glimantony.gmail.base.Base2DScreen;
