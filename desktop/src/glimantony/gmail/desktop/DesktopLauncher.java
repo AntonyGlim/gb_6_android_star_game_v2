14dda48c8d4c47b00c4b531b885089642da87769
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 10:40:42 +0300

    Сгенерирован проект утилитой LibGDX

---
+++ desktop/src/glimantony/gmail/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package glimantony.gmail.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import glimantony.gmail.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
