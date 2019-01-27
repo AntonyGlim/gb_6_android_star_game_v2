b1f544d12d1b6a8d0a042efa6604c12b9f2c0869
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 20:25:13 +0300

    Урок 4 - Добавлена конфигурация для DesktopLauncher

--- desktop/src/glimantony/gmail/desktop/DesktopLauncher.java
+++ desktop/src/glimantony/gmail/desktop/DesktopLauncher.java
@@ -9,6 +9,10 @@ import glimantony.gmail.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		float aspect = 3f / 4f;
+		config.width = 700;
+		config.height = (int) (config.width / aspect);
+		config.resizable = false; //Запрет на изменение ширины и высоты экрана
 		new LwjglApplication(new Star2DGame(), config);
 	}
 }
