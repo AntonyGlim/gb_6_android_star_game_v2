09d6635370b50e8cadf78d71d68c35159edf1929
Antony.Glim <Antony.Glim@gmail.com>
2019-01-27 20:54:05 +0300

    Урок 4 - Одинокая звезда без движения

--- core/src/glimantony/gmail/screen/MenuScreen.java
+++ core/src/glimantony/gmail/screen/MenuScreen.java
@@ -3,12 +3,14 @@ package glimantony.gmail.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import glimantony.gmail.base.Base2DScreen;
 import glimantony.gmail.math.Rect;
 import glimantony.gmail.sprites.Background;
+import glimantony.gmail.sprites.Star;
 
 /**
  * Класс отвечает за стартовое меню приложения
@@ -17,35 +19,42 @@ public class MenuScreen extends Base2DScreen {
 
     public static final float V_LEN = 0.001f; //величина вектора скорости
 
+    private TextureAtlas atlas;
     private Texture bg;
     private Background background;
+    private Star star;
 
     @Override
     public void show() { //проводим всю инициализацию
         super.show();
         bg = new Texture("textures/backgrounds/spase_stars_background.jpg");
         background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/mainAtlas.tpack"); //инициализируем конфиг для атласа
+        star = new Star(atlas); //звезда настраивает себя сама
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
         background.draw(batch); //фон сам знает как себя нарисовать
+        star.draw(batch);
         batch.end();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);//передаем новые границы мира, после их изменения (он сам изменит свои размеры)
+        star.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        atlas.dispose();
         super.dispose(); //Вызывается в конце
     }
 
--- core/src/glimantony/gmail/sprites/Star.java
+++ core/src/glimantony/gmail/sprites/Star.java
@@ -1,6 +1,34 @@
 package glimantony.gmail.sprites;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
 import glimantony.gmail.base.Sprite;
+import glimantony.gmail.math.Rect;
+import glimantony.gmail.math.Rnd;
 
 public class Star extends Sprite {
+
+    private Vector2 speed = new Vector2(); //ск-ть звезды
+    private Rect worldBounds; //границы мира
+    private float size = 0.01f; //размеры звезды
+
+    /**
+     * Принимает на вход атлас
+     * @param atlas
+     */
+    public Star(TextureAtlas atlas) {
+        super(atlas.findRegion("star"));
+        setHeightProportion(size);
+        speed.set(Rnd.nextFloat(-0.005f, 0.005f), Rnd.nextFloat(-0.5f, -0.1f)); //звежды летт вниз
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        this.worldBounds = worldBounds;
+        float posX = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight()); //звезда сгенерируется в произвольм месте на нашем поле
+        float posY = Rnd.nextFloat(worldBounds.getBottom(), worldBounds.getTop()); //звезда сгенерируется в произвольм месте на нашем поле
+        pos.set(posX, posY);
+    }
 }
