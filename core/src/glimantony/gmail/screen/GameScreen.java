6fed5b5d030ca9c68cb5ede6d7bd54a1383d773b
Antony.Glim <Antony.Glim@gmail.com>
2019-01-28 22:22:14 +0300

    Урок 5 - По кнопке ButtonPlay открываем новый экран и не висит приложение

--- core/src/glimantony/gmail/screen/GameScreen.java
+++ core/src/glimantony/gmail/screen/GameScreen.java
@@ -1,5 +1,7 @@
 package glimantony.gmail.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 
 import glimantony.gmail.base.Base2DScreen;
@@ -14,6 +16,12 @@ public class GameScreen extends Base2DScreen {
     @Override
     public void render(float delta) {
         super.render(delta);
+
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.end();
     }
 
     @Override
