4043c88c2fb7a57b3ba220da8572049485887d83
Antony.Glim <Antony.Glim@gmail.com>
2019-01-28 22:10:45 +0300

    Урок 5 - GameScreen заполнен

--- core/src/glimantony/gmail/screen/GameScreen.java
+++ core/src/glimantony/gmail/screen/GameScreen.java
@@ -1,6 +1,48 @@
 package glimantony.gmail.screen;
 
+import com.badlogic.gdx.math.Vector2;
+
 import glimantony.gmail.base.Base2DScreen;
+import glimantony.gmail.math.Rect;
 
 public class GameScreen extends Base2DScreen {
+    @Override
+    public void show() {
+        super.show();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return super.keyUp(keycode);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return super.touchDown(touch, pointer);
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        return super.touchUp(touch, pointer);
+    }
 }
