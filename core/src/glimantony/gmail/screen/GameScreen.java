41646b5f3d2bca5c550ecc84e9c7233269cbc42f
Antony.Glim <Antony.Glim@gmail.com>
2019-01-28 22:32:20 +0300

    Урок 5 - Новый экран - без кнопок и меьше звезд

--- core/src/glimantony/gmail/screen/GameScreen.java
+++ core/src/glimantony/gmail/screen/GameScreen.java
@@ -2,15 +2,35 @@ package glimantony.gmail.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import glimantony.gmail.base.Base2DScreen;
 import glimantony.gmail.math.Rect;
+import glimantony.gmail.sprites.Background;
+import glimantony.gmail.sprites.Star;
 
 public class GameScreen extends Base2DScreen {
+
+    private TextureAtlas atlas;
+    private Texture bg; //фон
+    private Background background; //обертка для фона
+    private Star[] stars; //массив звезд
+
+
     @Override
     public void show() {
         super.show();
+        bg = new Texture("textures/backgrounds/spase_stars_background.jpg");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack"); //инициализируем конфиг для атласа
+        stars = new Star[64]; //звезда настраивает себя сама
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+
     }
 
     @Override
@@ -25,7 +45,9 @@ public class GameScreen extends Base2DScreen {
      * @param delta
      */
     public void update(float delta){
-
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(delta);
+        }
     }
 
     /**
@@ -37,16 +59,26 @@ public class GameScreen extends Base2DScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
+        background.draw(batch); //фон сам знает как себя нарисовать
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].draw(batch);
+        }
         batch.end();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
+        background.resize(worldBounds);//передаем новые границы мира, после их изменения (он сам изменит свои размеры)
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
+        bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
