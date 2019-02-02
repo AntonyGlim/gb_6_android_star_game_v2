dd3433d98d7478b4b4f13a38c31a38940b42fbf0
Antony.Glim <Antony.Glim@gmail.com>
2019-02-02 20:49:37 +0300

    Урок 7 - От попадания корабль становиться красным

--- core/src/glimantony/gmail/sprites/game/Ship.java
+++ core/src/glimantony/gmail/sprites/game/Ship.java
@@ -21,6 +21,9 @@ public class Ship extends Sprite {
     protected float reloadInterval; //интервал стрельбы (время перезарядки)
     protected float reloadTimer; //для отсчета времени
 
+    private float damageInterval = 0.1f; //интервал повреждений
+    private float damageTimer = damageInterval; //для отсчета времени
+
     protected Sound shootSound; //звук выстрела
 
     protected Vector2 bulletSpeed; //пули летят в разные стороны у нас и у кораблей противников
@@ -44,6 +47,15 @@ public class Ship extends Sprite {
     }
 
     @Override
+    public void update(float delta) {
+        super.update(delta);
+        damageTimer += delta; //таймер тикает в бесконечность
+        if (damageTimer >= damageInterval){ //
+            frame = 0; //кадр всегда будет 0, кроме редких случаев, когда мы его сбросим (в корабль попали)
+        }
+    }
+
+    @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         this.worldBounds = worldBounds;
@@ -58,6 +70,15 @@ public class Ship extends Sprite {
     public void boom(){ //взрыв корабля
         Explosion explosion = explosionPool.obtain();
         explosion.set(getHeight(), pos); //размеры корабля и его местоположение
+    }
+
+    /**
+     * метод описывает повреждения
+     * @param damage - количество повреждений
+     */
+    public void damage(int damage){
+        frame = 1; //В корабль попали, меняем текстуру
+        damageTimer = 0; //пока таймер не увеличиться до damageInterval в методк update()
     }
 
     public void dispose() {
