2e39c86817af77c67ebf813feaced8fa841a90bf
Antony.Glim <Antony.Glim@gmail.com>
2019-02-02 10:36:06 +0300

    Урок 6 - Описана логика класса со взрывом

--- core/src/glimantony/gmail/sprites/game/Explosion.java
+++ core/src/glimantony/gmail/sprites/game/Explosion.java
@@ -1,7 +1,46 @@
 package glimantony.gmail.sprites.game;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
 import glimantony.gmail.base.Sprite;
 
+/**
+ * Класс описывает взрыв
+ */
 public class Explosion extends Sprite {
 
+    private float animatedInterval = 0.017f; //интервал смены кадров анимации
+    private float animatedTimer; //интервал смены кадров анимации
+
+    public Explosion(TextureRegion region, int rows, int cols, int frames) {
+        super(region, rows, cols, frames);
+    }
+
+    /**
+     * Взрыв будем доставать из пула объектов
+     * @param height - размеры взрыва в зависимости от размеров корабля
+     * @param pos - местонахождение взрыва
+     */
+    public void set(float height, Vector2 pos){
+        this.pos.set(pos); //описано в Sprite
+        setHeightProportion(height); //устанавливаем размеры
+    }
+
+    @Override
+    public void update(float delta) {
+        animatedTimer += delta;
+        if (animatedTimer >= animatedInterval){
+            animatedTimer = 0f;
+            if (++frame == regions.length){ //если последний кадр уже проигран
+                destroy(); //убираем с экрана
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        frame = 0; //чтобы начать с 0 кадра
+    }
 }
