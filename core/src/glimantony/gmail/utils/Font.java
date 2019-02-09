77bba411dd406a110e37451c8a08beb74223f587
Antony <Antony.Glim@gmail.com>
2019-02-09 17:44:20 +0300

    Урок 8 - Добавлено его описание

--- core/src/glimantony/gmail/utils/Font.java
+++ core/src/glimantony/gmail/utils/Font.java
@@ -2,5 +2,16 @@ package glimantony.gmail.utils;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
+/**
+ * В классе расширины некоторые функции базового класса для работы со шрифтами
+ */
 public class Font extends BitmapFont {
+    /**
+     * Работает с:
+     * @param fontFile - в нем заданы координаты расположения каждой буквы
+     * @param imageFile - сами буквы
+     */
+    public Font(String fontFile, String imageFile) {
+        super();
+    }
 }
