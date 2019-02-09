6a3ab947cda4b3ef47ae60ba1e2a624d1b11dad4
Antony <Antony.Glim@gmail.com>
2019-02-09 12:39:24 +0300

    Урок 7 - Описан класс кнопуи Game Over

--- core/src/glimantony/gmail/sprites/game/MessageGameOver.java
+++ core/src/glimantony/gmail/sprites/game/MessageGameOver.java
@@ -1,6 +1,13 @@
 package glimantony.gmail.sprites.game;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 import glimantony.gmail.base.Sprite;
 
 public class MessageGameOver extends Sprite {
+    public MessageGameOver(TextureAtlas atlas) {
+        super(atlas.findRegion("message_game_over"));
+        setHeightProportion(0.02f); //процент от высоты экрана
+        setBottom(0.02f); //смещение
+    }
 }
