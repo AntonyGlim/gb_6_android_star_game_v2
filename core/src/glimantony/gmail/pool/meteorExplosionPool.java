c0fe461f406a37ba7a3362aa0a4b270d19ea2287
Antony <Antony.Glim@gmail.com>
2019-02-11 23:01:38 +0300

    Думаю, что написал класс со взрывом метеорита

---
+++ core/src/glimantony/gmail/pool/meteorExplosionPool.java
@@ -0,0 +1,33 @@
+package glimantony.gmail.pool;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import glimantony.gmail.base.Sprite;
+import glimantony.gmail.base.SpritesPool;
+import glimantony.gmail.sprites.game.Explosion;
+
+public class meteorExplosionPool extends SpritesPool<Explosion> {
+    private Texture texture = new Texture("textures/meteorExplosion.png");
+    private TextureRegion region = new TextureRegion(texture);
+    private Sound explosionSound;
+
+    public meteorExplosionPool() {
+        this.explosionSound = Gdx.audio.newSound(Gdx.files.internal("sounds/explosion.mp3")); //TODO поменять музыку
+    }
+
+    @Override
+    protected Explosion newObject() {
+        return new Explosion(region, 3, 3, 9, explosionSound); //можно убедиться в этом открыв атлас
+    }
+
+    @Override
+    public void dispose() {
+        texture.dispose();
+        explosionSound.dispose();
+        super.dispose();
+    }
+}
